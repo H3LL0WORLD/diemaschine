c99f1ef644a43807b2ca8415f3be4942e8ef3728
moritz <moritz@moritz-t440s.Speedport_W_921V_1_43_000>
2017-03-22 21:42:58 +0100

    [cleaned everything up an added a new function]

--- diemaschine.py
+++ diemaschine.py
@@ -19,7 +19,7 @@ from threading import Thread
 import time
 from random import randint
 import csv
-
+import classifier
 
 SHODAN_API_KEY = "API_KEY"
 
@@ -41,21 +41,19 @@ def shodan_search(query):
 				#print "http://"+result['ip_str']+"/cam_1.cgi?.mjpg"
 	except shodan.APIError, e:
 	        print 'Error: %s' % e
-def add_known_face_db():
-	rnumber = (randint(0,8000000000))
-	#print rnumber
-	with open('knownfaces.csv', 'rt') as f:
-		reader = csv.reader(f, delimiter=',') 
-		for row in reader:
-			for field in row:
-				if field == rnumber:
-					is_in_file = True
-				else:
-					is_in_file = False
-	if is_in_file == True:
-		print rnumber + "ist schon da "
-	#else:
-    	#print rnumber + "ist noch nicht da"
+
+def database_thing(frame):
+	#this function should wait for the output of the classifier.infer function and either save the image in a new folder or add ist to the existing folder from one person
+	cv2.imwrite( "img.jpg", frame)
+	os.call("./classifier.py infer ./classifier.pkl img.jpg") #check if the face is known, this is very shitty i know but i'll change that
+	if 1==1:#if the face is knwon do nothing, if it isnt make a new person folder
+		print "The face is known"
+	else:
+		print "The face isn't known"
+		f_name="subject_" + number_of_faces
+		create_folder(fname)
+		#number_of_faces++
+		cv2.imwrite("./" +fname + "/img.jpg", frame)
 
 def cam_detect_faces(ip):
 	cascPath = "haarcascade_frontalface_default1.xml"
@@ -66,7 +64,7 @@ def cam_detect_faces(ip):
 	stream=urllib.urlopen(ip)
 	bytes=''
 	while True:
-	    bytes+=stream.read(1024)
+	    bytes+=stream.read(1024) 
 	    a = bytes.find('\xff\xd8')
 	    b = bytes.find('\xff\xd9')
 	    if a!=-1 and b!=-1:
@@ -79,7 +77,7 @@ def cam_detect_faces(ip):
 		        scaleFactor=1.9,
 		        minNeighbors=5,
 		        minSize=(30, 30),
-		        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+		        #flags=cv2.CV_HAAR_SCALE_IMAGE
 		    )
 			# Draw a rectangle around the faces
 		for (x, y, w, h) in faces:
@@ -87,6 +85,7 @@ def cam_detect_faces(ip):
 			localtime = time.asctime( time.localtime(time.time()) )
 			print "gesicht gefunden: ", localtime
 			cv2.imwrite( "pics/test"+localtime+".jpg", frame )
+			database_thing(frame)
 		    # Display the resulting frame
 
 		cv2.imshow('Video', frame)
@@ -98,49 +97,32 @@ def cam_detect_faces(ip):
 			exit(0)  
 
 def cam_detect_faces_once(ip):
-	cascPath = "haarcascade_frontalface_default.xml"
+	cascPath = "haarcascade_frontalface_default1.xml"
 	faceCascade = cv2.CascadeClassifier(cascPath)
 
 	video_capture = cv2.VideoCapture(0)
 
-	stream=urllib.urlopen(ip)
-	bytes=''
-	while True:
-	    bytes+=stream.read(1024)
-	    a = bytes.find('\xff\xd8')
-	    b = bytes.find('\xff\xd9')
-	    if a!=-1 and b!=-1:
-	        jpg = bytes[a:b+2]
-	        bytes= bytes[b+2:]
-	        frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.COLOR_BGR2GRAY)
-	        #cv2.imshow('i',frame)
-	        faces = faceCascade.detectMultiScale(
-		        frame,
-		        scaleFactor=1.9,
-		        minNeighbors=5,
-		        minSize=(30, 30),
-		        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-		    )
-			# Draw a rectangle around the faces
-		for (x, y, w, h) in faces:
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			localtime = time.asctime( time.localtime(time.time()) )
-			print "gesicht gefunden: ", localtime
-		    # Display the resulting frame
-		cv2.imshow('Video', frame)
 
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+	# Capture frame-by-frame
+	ret, frame = video_capture.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	faces = faceCascade.detectMultiScale(
+	    gray,
+	    scaleFactor=1.1,
+	    minNeighbors=5,
+	    minSize=(30, 30),
+	    #flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+	    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	    cv2.imwrite("img.jpg", frame)
+	# Display the resulting frame
+	cv2.imshow('Video', frame)
 
-		if cv2.waitKey(1) ==27:
-			exit(0) 
-		break 
-
-def create_folder(fname):
-	newpath = fname 
-	if not os.path.exists(newpath):
-		os.makedirs(newpath)
-	print newpath
+	# When everything is done, release the capture
+	video_capture.release()
+	cv2.destroyAllWindows()
 	        
 def view_webcam(ip):
 	stream=urllib.urlopen(ip)
@@ -157,71 +139,4 @@ def view_webcam(ip):
 	        if cv2.waitKey(1) ==27:
 	            exit(0)  
 
-def get_soup(url,header):
-    return BeautifulSoup(urllib2.urlopen(urllib2.Request(url,headers=header)),'html.parser')
-
-def get_images(name):
-	
-	query = name
-	image_type="ActiOn"
-	query= query.split()
-	query='+'.join(query)
-	url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
-	print url
-
-	DIR=".training-images"
-	header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
-	}
-	soup = get_soup(url,header)
-
-
-	ActualImages=[]# contains the link for Large original images, type of  image
-	for a in soup.find_all("div",{"class":"rg_meta"}):
-	    link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-	    ActualImages.append((link,Type))
-
-	print  "there are total" , len(ActualImages),"images"
-
-	if not os.path.exists(DIR):
-	            os.mkdir(DIR)
-	DIR = os.path.join(DIR, query.split()[0])
-
-	if not os.path.exists(DIR):
-	            os.mkdir(DIR)
-	###print images
-	for i , (img , Type) in enumerate( ActualImages):
-	    try:
-	        req = urllib2.Request(img, headers={'User-Agent' : header})
-	        raw_img = urllib2.urlopen(req).read()
-
-	        cntr = len([i for i in os.listdir(DIR) if image_type in i]) + 1
-	        print cntr
-	        if len(Type)==0:
-	            f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+".jpg"), 'wb')
-	        else :
-	            f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+"."+Type), 'wb')
-
-
-	        f.write(raw_img)
-	        f.close()
-	    except Exception as e:
-	        print "could not load : "+img
-	        print e
-#These are command to do things like training a new classifier to distinguish faces, at the moment please ignore them!
-#def pose_detection():
-#	call(["sudo python ./util/align-dlib.py .training-images/ align outerEyesAndNose .aligned-images/ --size 96"])
-#def get_represens():
-#	call(["sudo ./batch-represent/main.lua -outDir ./generated-embeddings/ -data ./aligned-images/"])
-
-#def train_model():
-#	call(["sudo python ./demos/classifier.py train ./generated-embeddings/"])
-
-
-#name = raw_input("name")
-#get_images(name)
-#cam_detect_faces("http://213.177.16.242:8080/cam_1.cgi?.mjpg")
-#shodan_search("webcamxp")
-#create_folder("test")
-#print(randint(0,100000000000000))
-#add_known_face_db()
-view_webcam("http://23.31.187.118:9000/cam_1.cgi?.mjpg")
+cam_detect_faces_once("http://23.31.187.118:9000/cam_1.cgi?.mjpg")
